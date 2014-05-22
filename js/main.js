87b1d57e73b11d1c262048940a9842b13590b5b2
Cassandra King <puddincat@gmail.com>
2014-05-22 19:41:27 -0400

    edit css

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@
 //    } );
 // } )( jQuery );
 // });
+
+
+// $(document).ready(function() {
+//   $('.slide').click(function() {
+//     $(this).parent().animate({width: '0'});
+//   });
+// });
+
+// $(document).ready(function() {
+//   $('.slide').click(function() {
+//     var $lefty = $(this).parent();
+//     $lefty.animate({
+//       left: parseInt($lefty.css('left'),10) == 0 ?
+//         -$lefty.outerWidth() :
+//         20
+//     });
+//   });
+// });
